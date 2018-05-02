1a2fd7dc8d899cb119482aa4732018b6a2d0b377
Jeuel Wilkerson <jbernardwilkerson@gmail.com>
2018-05-01 21:33:09 -0700

    import crons as es6

--- dist/helpers/cronJobs.js
+++ dist/helpers/cronJobs.js
@@ -1,18 +1,19 @@
 'use strict';
 
-var CronJob = require('cron').CronJob;
+var _cron = require('cron');
 
 module.exports = {
   makeChecker: function makeChecker() {
-    var checker = new CronJob('59 0 * * * *', function () {
+    // const checker = new CronJob('59 0 * * * *', () => {
+    var checker = new _cron.CronJob('* * * * * *', function () {
       console.log('ping');
     });
     return checker;
   },
   makeDaily: function makeDaily(cronFunction) {
-    var daily = new CronJob('0 0 7 * * *', cronFunction, function () {
+    var daily = new _cron.CronJob('0 0 7 * * *', cronFunction, function () {
       console.info('DAILY JOB HAS STOPPED!');
     }, false, 'America/Los_Angeles');
     return daily;
   }
-};
+}; // const CronJob = require('cron').CronJob;
