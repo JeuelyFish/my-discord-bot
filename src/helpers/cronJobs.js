db205f046d4a2295b8907050d9baffdb1fadabdc
Jeuel Wilkerson <jeuel@lytmus.com>
2018-05-17 14:07:25 -0700

    wip

--- src/helpers/cronJobs.js
+++ src/helpers/cronJobs.js
@@ -1,17 +1,49 @@
 import { CronJob, CronTime } from 'cron';
-import { random } from 'lodash';
+import { random, range, includes, get } from 'lodash';
 import { getGeneralChat, getRandomUser } from './common.js';
 import { purge } from './admin/commands.js';
 import { complimentRandomUser } from './compliments.js';
 
 
+const createSemiRandomTime = (dayInt, monthInt) => {
+    const timeArray = ['*'];
+    const monthsWith30Days = [3, 5, 8];
+    const monthsWith31Days = [0, 2, 4, 6, 7, 9, 10, 11]
+    const endOf30DayMonth = includes(monthsWith30Days, monthInt) && dayInt === 30;
+    const endOf31DayMonth = includes(monthsWith31Days, monthInt) && dayInt === 31;
+    const endOf28DayMonth = (monthInt === 1 && dayInt === 28);
+
+    if (endOf31DayMonth && monthInt === 11){
+        timeArray.unshift(1, 1)
+    } else if(endOf30DayMonth || endOf31DayMonth || endOf28DayMonth){
+        timeArray.unshift(monthInt + 1)
+        timeArray.unshift(1)
+    }  else {
+        timeArray.unshift(monthInt)
+        timeArray.unshift(dayInt + 1)
+    }
+    timeArray.unshift(random(range(7, 14))); // add hours
+    timeArray.unshift(random(range(0, 59))); // add minutes
+    timeArray.unshift(0); // add seconds
+    return timeArray.join(' ');
+}
+
+
+
 export const checker = () => {
-  const checkerCron = new CronJob('3 * * * * *',
+  const checkerCron = new CronJob('* * * * * *',
   function() {
     // console.log('ping');
-    // console.log(this.cronTime.source);
-    // console.log(this.nextDates());
-    //
+    const nextRunTime = this.nextDates().toDate();
+    const nextMonth = nextRunTime.getMonth();
+    const nextDay = nextRunTime.getDate();
+
+    console.log('nextRunTime: ', nextRunTime);
+    console.log('nextMonth: ', nextMonth);
+    console.log('nextDay: ', nextDay);
+
+
+
     // if(this.cronTime.source != '* * * * * *'){
     //   this.setTime(new CronTime('* * * * * *'));
     // }
